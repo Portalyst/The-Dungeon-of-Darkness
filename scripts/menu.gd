e7fbaa602453df10916f1c9c61922c56669c2b8c
Portalyst <steevex1977@gmail.com>
2025-04-18 22:41:56 +0500

    реворк меню часть 1

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,7 +1,20 @@
 extends Node2D
 
+var entering : bool = false
+
 func _on_button_mouse_entered():
-	$AnimationPlayer.play("play_area")
+	if entering == false:
+		$Menu/PlayNew.show()
+		
+	#$AnimationPlayer.play("play_area")
+
+func _process(delta: float) -> void:
+	if entering == true and $Camera2D.zoom.x != 1.5:
+		$Camera2D.zoom.x += 0.01
+		$Camera2D.zoom.y += 0.01
+		$"Menu/Big-shadow".modulate.a += 0.01
+	if $"Menu/Big-shadow".modulate.a >= 1:
+		get_tree().change_scene_to_file("res://scenes/level.tscn")
 
 func _on_button_1_mouse_entered():
 	$AnimationPlayer.play("setting_area")
@@ -10,9 +23,15 @@ func _on_button_2_mouse_entered():
 	$AnimationPlayer.play("exit_area")
 
 func _on_button_pressed():
-	$Menu/Play/CPUParticles2D.emitting = true
-	$AnimationPlayer.play("play_area")
-	$Menu/Play/Timer.start()
+	if $Menu/PlayNew.visible == true and entering == false:
+		$Logo.hide()
+		$Logo2.hide()
+		entering = true
+		$Menu/PlayNew.hide()
+		
+		#$Menu/Play/CPUParticles2D.emitting = true
+		#$AnimationPlayer.play("play_area")
+		#$Menu/Play/Timer.start()
 
 func _on_button_2_pressed():
 	$Menu/Exit/CPUParticles2D.emitting = true
@@ -26,3 +45,7 @@ func _on_button_1_pressed():
 
 func _on_timer_timeout():
 	get_tree().change_scene_to_file("res://scenes/level.tscn")
+
+
+func _on_button_mouse_exited() -> void:
+	$Menu/PlayNew.hide()
